31de847ffb70d219682631427fbfdd80a5c33064
Jesus Alvarez Sombrerero <93137954+dontloseyourheadsu@users.noreply.github.com>
2023-10-17 22:30:22 -0600

    min change

--- DynamicProgrammingJava/src/main/java/org/example/Main.java
+++ DynamicProgrammingJava/src/main/java/org/example/Main.java
@@ -63,6 +63,33 @@ public class Main {
         return false;
     }
 
+    public static int minChange(int amount, List<Integer> coins) {
+        return minChange(amount, coins, new HashMap<>());
+    }
+
+    public static int minChange(int amount, List<Integer> coins, HashMap<Integer, Integer> memo) {
+        if (amount == 0) return 0;
+
+        if (amount < 0) return -1;
+
+        if (memo.containsKey(amount)) return memo.get(amount);
+
+        int minCoins = -1;
+        for (int coin: coins) {
+            int subAmount = amount - coin;
+            int subCoins = minChange(subAmount, coins, memo);
+            if (subCoins != -1) {
+                int numCoins = subCoins + 1;
+                if (numCoins < minCoins || minCoins == -1) {
+                    minCoins = numCoins;
+                }
+            }
+        }
+
+        memo.put(amount, minCoins);
+        return minCoins;
+    }
+
     public static void main(String[] args) {
         System.out.println(fib(8));
         System.out.println(tribonacci(8));
