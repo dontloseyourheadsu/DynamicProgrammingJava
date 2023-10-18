5a53fcd617656d3d48da0e47cad60e5e66419d74
Jesus Alvarez Sombrerero <93137954+dontloseyourheadsu@users.noreply.github.com>
2023-10-17 22:02:26 -0600

    Tribonacci

--- DynamicProgrammingJava/src/main/java/org/example/Main.java
+++ DynamicProgrammingJava/src/main/java/org/example/Main.java
@@ -20,7 +20,24 @@ public class Main {
         return result;
     }
 
+    public static int tribonacci(int n) {
+        return tribonacci(n, new HashMap<>());
+    }
+
+    public static int tribonacci(int n, HashMap<Integer, Integer> memo) {
+        if (n == 0 || n == 1) return n;
+        if (n == 2) return 1;
+
+        if (memo.containsKey(n)) return memo.get(n);
+
+        int result = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo);
+        memo.put(n, result);
+
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println(fib(5));
+        System.out.println(fib(8));
+        System.out.println(tribonacci(8));
     }
 }
