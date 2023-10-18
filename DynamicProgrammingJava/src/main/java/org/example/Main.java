7b2d7f9cbeb471ff1ebd824011c19ea53b90e26e
Jesus Alvarez Sombrerero <93137954+dontloseyourheadsu@users.noreply.github.com>
2023-10-17 21:57:46 -0600

    Fibonacci

---
+++ DynamicProgrammingJava/src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static int fib(int n) {
+        return fib(n, new HashMap<>());
+    }
+    public static int fib(int n, HashMap<Integer, Integer> memo) {
+        if (n == 0 || n == 1) return n;
+
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+
+        int result = fib(n - 1, memo) + fib(n - 2, memo);
+        memo.put(n, result);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(5));
+    }
+}
